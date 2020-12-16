1cb88fb80d3e306cd536441ff64499a7cb620170
Ryan Giordano <rgiordan@gmail.com>
2020-12-15 19:34:00 -0800

    delete unused python functions

--- regression_sensitivity/regsens_rgiordandev/__init__.py
+++ regression_sensitivity/regsens_rgiordandev/__init__.py
@@ -1,4 +1,3 @@
 from .regression_sensitivity_lib import \
     reg, reg_obj, get_standard_error_matrix, get_regression_w_grads, \
-    get_regression_offset_grads, \
-    inds_to_effect_change, print_change_results
+    get_regression_offset_grads
--- regression_sensitivity/regsens_rgiordandev/regression_sensitivity_lib.py
+++ regression_sensitivity/regsens_rgiordandev/regression_sensitivity_lib.py
@@ -191,32 +191,3 @@ def get_regression_offset_grads(beta, y, x, offset0, se_group=None):
     se_grad = autograd.jacobian(get_se)(offset0)
 
     return se, betahat_grad, se_grad
-
-
-
-##########################################################
-# The below functions are now being done in the R library.
-
-# Estimate how many datapoints we would have to remove to effect a change of delta.
-def inds_to_effect_change(leverage, desired_delta):
-    # Argsort sorts low to high.
-    # We are removing points, so multiply by -1.
-    sort_inds = np.argsort(leverage * np.sign(desired_delta))
-    deltas = -1 * np.cumsum(leverage[sort_inds])
-    change_sign_inds = np.argwhere(
-        np.sign(desired_delta) * (desired_delta - deltas) <= 0.)
-    if len(change_sign_inds) > 0:
-        first_ind_change_sign = np.min(change_sign_inds)
-        remove_inds = sort_inds[:(first_ind_change_sign + 1)]
-        return remove_inds
-    else:
-        return None
-
-
-def print_change_results(inds, effect_str, lev_len):
-    print('Assuming linearity, which may not hold for large numbers of points,')
-    if inds is not None:
-        print('removing {} observations ({:0.2f}%) would {}.'.format(
-               len(inds), 100 * len(inds) / lev_len, effect_str))
-    else:
-        print('no number of observations would {}.'.format(effect_str))
