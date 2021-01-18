9d3c3b25b2485bba8295c4d93297b9e43fed9619
Ryan Giordano <rgiordan@gmail.com>
2021-01-18 14:52:26 -0800

    test se group bug

--- zaminfluence/tests/testthat/test_components.R
+++ zaminfluence/tests/testthat/test_components.R
@@ -8,12 +8,44 @@ library(tidyverse)
 
 context("zaminfluence")
 
+AssertNearlyEqual <- function(x, y, tol=1e-9) {
+  diff_norm <- max(abs(x - y))
+  info_str <- sprintf("%e > %e", diff_norm, tol)
+  expect_true(diff_norm < tol, info=info_str)
+}
+
+GetSandwichCov <- function(fit, se_group=NULL) {
+  if (is.null(se_group)) {
+    return(vcov(fit))
+  } else {
+    return(vcovCL(fit, cluster=se_group, type="HC0", cadjust=FALSE))
+  }
+}
+
 
 test_that("se groups can be non-ordered", {
-  df <- GenerateIVRegressionData(100, 0.5, num_groups=10)
+  num_obs <- 100
+  df <- GenerateIVRegressionData(num_obs, 0.5, num_groups=10)
   iv_res <- ivreg(y ~ x1 + 1 | z1 + 1, data=df, x=TRUE, y=TRUE)
   reg_res <- lm(y ~ x1 + 1, data=df, x=TRUE, y=TRUE)
 
-  ComputeRegressionResults(reg_res)
-  ComputeIVRegressionResults(iv_res)
+  TestSEGroup <- function(se_group) {
+    reg_zam <- ComputeRegressionResults(reg_res, se_group=se_group)
+    iv_zam <- ComputeIVRegressionResults(iv_res, se_group=se_group)
+
+    # The coefficients shouldn't depend on se_group, but test for good measure
+    AssertNearlyEqual(reg_zam$betahat, reg_res$coefficients)
+    AssertNearlyEqual(iv_zam$betahat, iv_res$coefficients)
+
+    AssertNearlyEqual(reg_zam$se_mat, GetSandwichCov(reg_res, se_group=se_group))
+    AssertNearlyEqual(iv_zam$se_mat, GetSandwichCov(iv_res, se_group=se_group))
+  }
+
+  TestSEGroup(NULL)
+  ordered_groups <- rep(1:20, each=5)
+  TestSEGroup(ordered_groups)
+  TestSEGroup(ordered_groups + 50)
+  TestSEGroup((ordered_groups + 2) * 2)
+  TestSEGroup(ordered_groups[sample(num_obs, replace=TRUE)])
+  TestSEGroup(ordered_groups[sample(num_obs)])
 })
