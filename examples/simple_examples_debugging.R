7edee3da83e31e1521e3dbf394356b3ef25ca91f
Ryan Giordano <rgiordan@gmail.com>
2021-09-08 21:34:30 -0700

    Writing out the structure

--- examples/simple_examples_debugging.R
+++ examples/simple_examples_debugging.R
@@ -38,6 +38,44 @@ reg_fit <- lm(data = df, formula = reg_form, x=TRUE, y=TRUE)
 
 # Get influence.
 reg_infl <- ComputeModelInfluence(reg_fit)
+
+# The main object is a Model Gradients object.  It is a list and must have
+# - weights:    The original data weights
+# - betahat:   The original estimator (length D)
+# - sehat:      The original standard errors (length D)
+# - n_obs:      The original number of observations
+#
+# - beta_grad:  A matrix (n_obs x D) of beta gradients
+# - se_grad:    A matrix (n_obs x D) of se gradients
+#
+# For a regression, it must also have
+# - regressor_names:    The names of the regressors
+# - model_fit:          Everything you need to re-run stuff
+
+# Then maybe a parameter inference object?  Need a better name.
+# It has a:
+# - target_index:   The index into betahat
+# - sig_num_ses:    The number of ses that form a confidence interval
+# - beta, beta_mzse, beta_pzse:  Influence summary objects
+
+# Influence summary objects contain an processed influence vector.
+# They are the influence scores for a single quantity of interest.
+# They are created with ProcessInfluenceVector().  Then contain
+# - base_value:     The original value of the quantity of interest
+# - neg, pos:       Sorted influence scores for the negative and positive 
+#                    influence scores, where.
+# Sorted influence scores have
+# - infl_inds:      Indices into the original data that sort the influence
+#                   scores of the corresponding sign.  E.g., infl_inds[1]
+#                   for the `neg` entry is the index of the most negative
+#                   influence score.
+# - infl_cumsum:    The cumulative sum of the sorted influences scores with
+#                   the specified sign.
+# - num_obs:        The total number of observations (is this necessary?)
+# - obs_per_row:    The number of observations per row (is this necessary?)
+
+
+
 AppendTargetRegressorInfluence <- function(reg_infl, target_regressor,
                                            sig_num_ses=qnorm(0.975)) {
     if (is.null(reg_infl[["targets"]])) {
@@ -47,7 +85,6 @@ AppendTargetRegressorInfluence <- function(reg_infl, target_regressor,
     if (length(target_index) != 1) {
         stop("Error finding target regressor in the regression.")
     }
-    
 
     se_grad <- reg_infl$weights * reg_infl$se_grad[target_index,]
     beta_grad <- reg_infl$weights * reg_infl$beta_grad[target_index, ]
@@ -85,6 +122,14 @@ influence_dfs <- SortAndAccumulate(grad_df)
 
 # Compute the AMIP and friends for changes of sign and significance.
 target_change <- GetRegressionTargetChange(influence_dfs, "prop_removed")
+
+# Then there are signals, which are changes in quantities of interest
+# that produce certain changes.  A signal can have
+# - metric:         The name of the quantity of interest
+# - signal:         The amount to change the quantity of interest
+# - description:    A summary of what the change means
+
+# TODO: Should we also keep rerun and apip information in the signals?
 
 GetRegressionSignals <- function(betahat, beta_mzse, beta_pzse) {
     sign_label <- "sign"
